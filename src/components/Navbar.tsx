8e2df15e1aacdc5a0b5fddd55c6b81c1b2c4ade2
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-02-07 10:49:51 +0100

    feat: navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,29 @@
+"use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import Image from 'next/image'
+import Image from "next/image";
 import { ModeToggle } from "./ModeToggle";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav className="bg-primary text-primary-foreground top-0 sticky flex flex-nowrap z-10">
       <div className="container mx-auto px-4">
-        <div className="flex items-center h-16 gap-8">
-          <div className="gap-2">
-            <Link href="/" className="text-xl font-bold flex flex-row justify-center items-center gap-2">
-              <Image src='/logo.png' alt="icon" width={40} height={40} />
+        <div className="flex items-center h-16 justify-between">
+          <div className="flex items-center gap-8">
+            <Link
+              href="/"
+              className="md:text-xl font-bold flex flex-row justify-center items-center gap-2"
+            >
+              <Image src="/logo.png" alt="icon" width={40} height={40} />
               Time Logger
             </Link>
-          </div>
-          <div className="flex">
-            <Button variant="ghost">
-              <Link href="/">Dashboard</Link>
-            </Button>
+            <div className="flex gap-6">
+              <Button variant={pathname === "/" ? "secondary" : "ghost"}>
+                <Link href="/">Dashboard</Link>
+              </Button>
+            </div>
           </div>
           <ModeToggle />
         </div>
